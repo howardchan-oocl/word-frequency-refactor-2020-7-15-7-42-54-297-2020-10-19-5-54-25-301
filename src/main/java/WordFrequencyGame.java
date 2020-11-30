3a2d38bb9dbf22da9811e79a2357d8ecb508838b
chanho <howard.chan@oocl.com>
2020-11-30 16:57:47 +0800

    [Word Frequency Refactoring][Howard Chan]refactor:optimize import, extract constant, format code, rename variable

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -1,76 +1,56 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.StringJoiner;
-import java.io.CharArrayWriter;
-
-import java.time.LocalDateTime;
+import java.util.*;
 
 public class WordFrequencyGame {
-    public String getResult(String inputStr){
 
+    public static final String WHITE_SPACE_REGEX = "\\s+";
 
-        if (inputStr.split("\\s+").length==1) {
-            return inputStr + " 1";
-        } else {
+    public String getResult(String sentence) {
+        try {
+            //split the input string with 1 to n pieces of spaces
+            String[] arr = sentence.split(WHITE_SPACE_REGEX);
 
-            try {
-
-                //split the input string with 1 to n pieces of spaces
-                String[] arr = inputStr.split("\\s+");
-
-                List<Input> inputList = new ArrayList<>();
-                for (String s : arr) {
-                    Input input = new Input(s, 1);
-                    inputList.add(input);
-                }
-
-                //get the map for the next step of sizing the same word
-                Map<String, List<Input>> map =getListMap(inputList);
-
-                List<Input> list = new ArrayList<>();
-                for (Map.Entry<String, List<Input>> entry : map.entrySet()){
-                    Input input = new Input(entry.getKey(), entry.getValue().size());
-                    list.add(input);
-                }
-                inputList = list;
-
-                inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
-
-                StringJoiner joiner = new StringJoiner("\n");
-                for (Input w : inputList) {
-                    String s = w.getValue() + " " +w.getWordCount();
-                    joiner.add(s);
-                }
-                return joiner.toString();
-            } catch (Exception e) {
-
-
-                return "Calculate Error";
+            List<Input> inputList = new ArrayList<>();
+            for (String s : arr) {
+                Input input = new Input(s, 1);
+                inputList.add(input);
             }
+
+            //get the map for the next step of sizing the same word
+            Map<String, List<Input>> map = getListMap(inputList);
+
+            List<Input> list = new ArrayList<>();
+            for (Map.Entry<String, List<Input>> entry : map.entrySet()) {
+                Input input = new Input(entry.getKey(), entry.getValue().size());
+                list.add(input);
+            }
+            inputList = list;
+
+            inputList.sort((word1, word2) -> word2.getWordCount() - word1.getWordCount());
+
+            StringJoiner joiner = new StringJoiner("\n");
+            for (Input w : inputList) {
+                String s = w.getValue() + " " + w.getWordCount();
+                joiner.add(s);
+            }
+            return joiner.toString();
+        } catch (Exception e) {
+            return "Calculate Error";
         }
+
     }
 
-
-    private Map<String,List<Input>> getListMap(List<Input> inputList) {
+    private Map<String, List<Input>> getListMap(List<Input> inputList) {
         Map<String, List<Input>> map = new HashMap<>();
-        for (Input input :  inputList){
+        for (Input input : inputList) {
 //       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-            if (!map.containsKey(input.getValue())){
+            if (!map.containsKey(input.getValue())) {
                 ArrayList arr = new ArrayList<>();
                 arr.add(input);
                 map.put(input.getValue(), arr);
-            }
-
-            else {
+            } else {
                 map.get(input.getValue()).add(input);
             }
         }
-
-
         return map;
     }
-
-
 }
