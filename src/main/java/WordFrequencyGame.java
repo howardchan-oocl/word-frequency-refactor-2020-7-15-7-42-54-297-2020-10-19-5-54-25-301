66368f248f74e7a678fd59f874db1def139fc68d
chanho <howard.chan@oocl.com>
2020-12-01 10:47:07 +0800

    [Word Frequency Refactoring][Howard Chan]refactor:separate the complex stream logic

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -2,21 +2,29 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class WordFrequencyGame {
-
     public static final String WHITE_SPACE_REGEX = "\\s+";
     public static final String NEXT_LINE = "\n";
 
     public String getResult(String sentence) throws CalculateErrorException {
         try {
-            List<WordFrequency> wordFrequencyList = Arrays.stream(sentence.split(WHITE_SPACE_REGEX))
-                    .distinct().map(word -> new WordFrequency(word, Collections.frequency(Arrays.asList(sentence.split(WHITE_SPACE_REGEX)), word)))
-                    .sorted((word1, word2) -> word2.getCount() - word1.getCount())
-                    .collect(Collectors.toList());
+            List<String> words = splitSentenceIntoWordList(sentence);
+
+            List<WordFrequency> wordFrequencyList = getSortedWordFrequencyList(words);
 
             return buildResult(wordFrequencyList);
         } catch (Exception exception) {
             throw new CalculateErrorException();
         }
+    }
+
+    private List<String> splitSentenceIntoWordList(String sentence) {
+        return Arrays.asList(sentence.split(WHITE_SPACE_REGEX));
+    }
+
+    private List<WordFrequency> getSortedWordFrequencyList(List<String> words){
+        return words.stream().distinct().map(word -> new WordFrequency(word, Collections.frequency(words, word)))
+                .sorted((word1, word2) -> word2.getCount() - word1.getCount())
+                .collect(Collectors.toList());
     }
 
     private String buildResult(List<WordFrequency> wordFrequencyList) {
